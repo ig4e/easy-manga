7d4c67482ffb79dbf351be03b576fb2da37787b6
Sekai <50212311+ig4e@users.noreply.github.com>
2022-09-15 18:19:31 +0200

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,25 @@
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { MangaModule } from './manga/manga.module';
-import { ChaptersModule } from './chapters/chapters.module';
-import { SourcesDataModule } from './sources-data/sources-data.module';
-import { MangaReaderService } from './sources-data/manga-reader/manga-reader.service';
-import { MadaraService } from './sources-data/madara/madara.service';
-import { GenresModule } from './genres/genres.module';
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { MangaModule } from "./manga/manga.module";
+import { ChaptersModule } from "./chapters/chapters.module";
+import { SourcesDataModule } from "./sources-data/sources-data.module";
+import { MangaReaderService } from "./sources-data/manga-reader/manga-reader.service";
+import { MadaraService } from "./sources-data/madara/madara.service";
+import { GenresModule } from "./genres/genres.module";
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: true,
-    }),
-    MangaModule,
-    ChaptersModule,
-    SourcesDataModule,
-    GenresModule,
-  ],
-  providers: [MangaReaderService, MadaraService],
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: true,
+            introspection: true,
+        }),
+        MangaModule,
+        ChaptersModule,
+        SourcesDataModule,
+        GenresModule,
+    ],
+    providers: [MangaReaderService, MadaraService],
 })
 export class AppModule {}
