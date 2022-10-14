65b66c9a9721ba527ed422aeb5ab754da5366da5
Sekai <50212311+ig4e@users.noreply.github.com>
2022-10-14 18:44:38 +0200

    ehe

--- src/manga/manga.service.ts
+++ src/manga/manga.service.ts
@@ -131,7 +131,6 @@ export class MangaService {
                 (relation) => relation.type === "artist",
             );
 
-            console.log(dexResult);
 
             return {
                 dexId: dexResult.id,
